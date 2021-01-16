900b37ccd1e00ea83dbcb4eb0d76db868e602a8a
jake <jacob.perman@gmail.com>
2021-01-15 19:25:03 -0600

    pycasso doneee

--- 8.3_Adventure.py
+++ 8.3_Adventure.py
@@ -6,3 +6,24 @@ ADVENTURE PROGRAM
 3.) Expand your program to make it a real adventure game
 
 '''
+
+
+room_list = []
+# Room parameters - ["Description", N, S, E, W]
+room = ["You are in the main chamber of the dungeon", ]
+room_list.append(room)
+room = ["", None, None, None, None]
+room_list.append(room)
+room = ["", None, None, None, None]
+room_list.append(room)
+room = ["", None, None, None, None]
+room_list.append(room)
+room = ["", None, None, None, None]
+room_list.append(room)
+
+#spawn player
+current_room = 0
+
+done = False
+while not done:
+    
